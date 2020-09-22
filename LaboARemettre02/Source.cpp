f7ac3c50d99980bd46d81a79bc4e902ad7a2b7d1
HowlingOkami <70543718+HowlingOkami@users.noreply.github.com>
2020-09-22 12:25:14 -0400

    Update Source.cpp
    j'ai mit la version avec un petit plan de test

--- LaboARemettre02/Source.cpp
+++ LaboARemettre02/Source.cpp
@@ -1,7 +1,8 @@
-//But: Écrire un programme pour calculer le montant qu’un individu devra débourser pour faire un voyage dans une automobile louée. 
-//L’algorithme tient compte du kilométrage parcouru et de la durée du voyage qui sont soumises par le requérant.
+//But: Ã‰crire un programme pour calculer le montant quâ€™un individu devra dÃ©bourser pour faire un voyage dans une automobile louÃ©e. 
+//Lâ€™algorithme tient compte du kilomÃ©trage parcouru et de la durÃ©e du voyage qui sont soumises par le requÃ©rant.
 //Auteur: Thomas Berube
 //Date: 17/09/2020
+
 
 #include <iostream>
 
@@ -9,15 +10,15 @@ int main()
 {
 	setlocale(LC_ALL, "");
 	//Declaration des variables
-	int nb1; //duree du voyage en jour
-	int nb2; // Distance du voyage
-	int nb3; //nombre de km gratuit
-	int nb4; //cout de location total
-	int nb5; //litres de gas necessaire
-	int nb6; //prix du gas
-	int nb7; //prix du km d'extra
-	int nb8; //km d'extra
-	int nb9; //Prix total
+	float nb1; //duree du voyage en jour
+	float nb2; // Distance du voyage
+	float nb3; //nombre de km gratuit
+	float nb4; //cout de location total
+	float nb5; //litres de gas necessaire
+	float nb6; //prix du gas
+	float nb7; //prix du km d'extra
+	float nb8; //km d'extra
+	float nb9; //Prix total
 
 	//Voir le prix pour la duree seulement
 	std::cout << " Veuillez rentrer la duree en jours du voyage ";
@@ -30,6 +31,11 @@ int main()
 	nb4 = nb1 * 45;
 	nb6 = nb5 * 1.25;
 	nb8 = nb2 - nb3;
+	
+	if (nb1 >= 1 && nb2 >= 1)
+	{
+
+	
 	//Mesurer prix du km extra
 	if (nb8 > 0)
 	{
@@ -40,7 +46,10 @@ int main()
 	{
 		nb7 = 0;
 	}
+	
 	nb9 = (nb4 + nb6 + nb7);
+
+
 
 	std::cout << "nombre de KM gratuit = " << nb3 << " KM" << std::endl;
 	std::cout << "Prix de la location seul = " << nb4 << " $" << std::endl;
@@ -48,6 +57,20 @@ int main()
 	std::cout << " Prix de l'essence = " << nb6 << " $ " << std::endl;
 	std::cout <<  " Cout extra du km" << nb7 << " $ " << std::endl;
 	std::cout << " Le prix total est de " << nb9 << " $ " << std::endl;
-
+}
+	else
+	{
+		std::cout << "Erreur, veullier rentrer des valeurs positives ou a valeur minimale de 1";
+	}
+	/*
+	plan de test
+	jour	distance	resultat tot
+	10		250			464.25
+	1		1			45.095
+	0		1			Erreur, veullier rentrer des valeurs positives ou a valeur minimale de 1
+	0		0			Erreur, veullier rentrer des valeurs positives ou a valeur minimale de 1
+	-5		-4			Erreur, veullier rentrer des valeurs positives ou a valeur minimale de 1
+	
+	*/
 	return 0;
 }
